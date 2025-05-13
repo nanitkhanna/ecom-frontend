9c25e63ec5c40d87cc95fa5a13b6ea3876cf2e90
nanitkhanna <nanitkhanna03@gmail.com>
2025-05-13 18:45:16 +0530

    Initial commit, configuring dependencies

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { FaBeer } from 'react-icons/fa'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div
+      className='flex items-center justify-center h-screen bg-gray-800 text-white
+       text-2xl font-bold'>
+        Welcome <FaBeer />
+    </div>
+  )
+}
+
+export default App
